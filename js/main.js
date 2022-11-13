751d32032d83f9537dfff699d09d3e5fc7a3f9fb
Mahdy Luqman <mahdyluqman786@gmail.com>
2022-11-13 15:27:46 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -100,7 +100,17 @@
     
 })(jQuery);
 
-document.querySelector(".banner__close").addEventListener("click", function () {
-  this.closest(".banner").style.display = "none";
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
 });
 
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
